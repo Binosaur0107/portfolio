6894c705780c76eca886b572c9a7de4f95a9e5f3
Arvy <arvymagimot07@gmail.com>
2025-11-24 05:13:07 +0800

    portfolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 
 export default function App() {
   const [activeSection, setActiveSection] = useState("home");
