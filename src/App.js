5266bbde6ab71a2581c127f427c8878ab5843712
Arvy <arvymagimot07@gmail.com>
2025-11-24 07:21:09 +0800

    portfolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact  from "./components/Contacts";
+import Contact from "./components/Contacts";
 
 export default function App() {
 
