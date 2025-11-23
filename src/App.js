7b77690cfce04bbf06da6a1b1fa5b8436ccd8315
Arvy <arvymagimot07@gmail.com>
2025-11-24 07:18:39 +0800

    Fix component file names

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Contact  from "./components/Contacts";
 
 export default function App() {
-  
+
   return (
     <>
       <Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <header className="navbar">
+      
       <nav className="menu">
         <a href="#home">Home</a>
         <a href="#about">About Me</a>
